56fd714217ac52e1b0897fe4b2641433b59c38d7
Carlton <22f3001919@ds.study.iitm.ac.in>
2025-05-21 20:57:20 +0530

    Add CORS middleware and update README with demo endpoint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # This is a vercel demo
 Some endpoint functions will be demoed.
+https://tds-2025-t2-vexp.vercel.app/api/params?hobby=tds&time=3
--- api/index.py
+++ api/index.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI ()
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get ("/")
 def index ():
@@ -8,3 +17,15 @@ def index ():
         "messsage": "Welcome to the FastAPI application!",
     }
 
+@app.get ("/api/params")
+def search (request: Request):
+    parameters = list ()
+
+    for parameter_name in request.query_params.keys ():
+        parameters.append (parameter_name)
+
+    print (parameters)
+
+    return {
+        "parameters": parameters,
+    }
