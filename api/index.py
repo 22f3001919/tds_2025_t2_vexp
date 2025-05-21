9325a507eeece282e14d174c6cc0fe99becf8f7f
Carlton <22f3001919@ds.study.iitm.ac.in>
2025-05-21 21:04:56 +0530

    Refactor search endpoint to properly append parameter names and values from query parameters

--- api/index.py
+++ api/index.py
@@ -20,9 +20,17 @@ def index ():
 @app.get ("/api/params")
 def search (request: Request):
     parameters = list ()
+    parameter_values = list ()
 
     for parameter_name in request.query_params.keys ():
-        parameters.append (parameter_name)
+        
+        parameter_values = request.query_params.getlist (parameter_name)
+        for value in parameter_values:
+            parameters.append ({
+                "name": parameter_name,
+                "value": value
+            })
+
 
     print (parameters)
 
